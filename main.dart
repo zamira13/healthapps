e360017e8f3acff16816d4e9e0a72b0d0c5871b4
Denipratama01 <158029466+Denipratama01@users.noreply.github.com>
2024-11-06 20:13:03 +0700

    update file main
    halaman ini sebagai  splash screen saat kita masuk sebelum delay 3 detik dan masuk ke login

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // Mengimpor file firebase_options.dart
+import 'login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
